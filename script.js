aac3d7d16f78dfcc3702634cae87ea5db389abce
Banbarashik <odinokyskitalec@yahoo.com>
2021-12-08 00:32:37 +0700

    implement appending book cards to the DOM

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ function addBookToLibrary() {
 
   const newBook = new Book(title, author, pages);
   myLibrary.push(newBook);
+
+  myLibrary.forEach((book) => {
+    const bookCardBody = document.createElement("article");
+    const title = document.createElement("h2");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    bookCardBody.appendChild(title);
+    bookCardBody.appendChild(author);
+    document.body.appendChild(bookCardBody);
+  });
 }
 
 const addBookBtn = document.getElementById("add-book");
