66649554c37ba318807f05bfe926a61602694869
Banbarashik <odinokyskitalec@yahoo.com>
2021-12-08 20:48:27 +0700

    added checkboxes to cards

--- script.js
+++ script.js
@@ -17,11 +17,21 @@ function addBookToLibrary() {
   const newBook = new Book(title, author, pages);
   myLibrary.push(newBook);
 
-  myLibrary.forEach((book) => {
+  myLibrary.forEach((book, index) => {
     const bookCardBody = document.createElement("article");
     const title = document.createElement("h2");
     const author = document.createElement("p");
     const pages = document.createElement("p");
+    const formCheckbox = document.getElementById("status");
+    const inputLabelContainer = document.createElement("div");
+    inputLabelContainer.classList.add("input-and-label");
+    const statusInput = document.createElement("input");
+    const statusLabel = document.createElement("label");
+    statusInput.setAttribute("type", "checkbox");
+    statusInput.setAttribute("id", "read" + index);
+    statusLabel.setAttribute("for", "read" + index);
+    statusLabel.textContent = "Read";
+    if (formCheckbox.checked === true) statusInput.checked = true;
     bookCardBody.classList.add("book-card");
     title.classList.add("title");
     author.classList.add("author");
@@ -32,6 +42,9 @@ function addBookToLibrary() {
     bookCardBody.appendChild(title);
     bookCardBody.appendChild(author);
     bookCardBody.appendChild(pages);
+    inputLabelContainer.appendChild(statusInput);
+    inputLabelContainer.appendChild(statusLabel);
+    bookCardBody.appendChild(inputLabelContainer);
     cardsBlock.appendChild(bookCardBody);
   });
 }
