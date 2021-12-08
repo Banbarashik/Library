cce0954a6a47d2925b452ed604da8f5d68309660
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-08 17:17:44 +0700

    started setting up the  layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages) {
   this.title = title;
@@ -8,6 +8,7 @@ function Book(title, author, pages) {
 }
 
 function addBookToLibrary() {
+  myLibrary = [];
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
@@ -20,11 +21,16 @@ function addBookToLibrary() {
     const title = document.createElement("h2");
     const author = document.createElement("p");
     const pages = document.createElement("p");
+    bookCardBody.classList.add("book-card");
+    title.classList.add("title");
+    author.classList.add("author");
+    pages.classList.add("pages");
     title.textContent = book.title;
     author.textContent = book.author;
     pages.textContent = book.pages;
     bookCardBody.appendChild(title);
     bookCardBody.appendChild(author);
+    bookCardBody.appendChild(pages);
     document.body.appendChild(bookCardBody);
   });
 }
