6957cea6aa6fb036af04f28eff7c6ecede0518b6
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-11 03:28:44 +0700

    wrote code to create new arrays if the previous array has length = 8. the previous array stays still, only new fill in

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// insert goThoughArray() inside .forEach
 "use strict";
-let myLibrary = [];
+let myLibrary = [[]];
 const cardsBlock = document.querySelector(".cards");
 const addBookBtn = document.getElementById("add-book");
+let isFull;
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -11,7 +13,7 @@ function Book(title, author, pages, readStatus) {
 }
 
 function addBookToLibrary() {
-  cardsBlock.replaceChildren();
+  // cardsBlock.replaceChildren();
 
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
@@ -20,12 +22,40 @@ function addBookToLibrary() {
 
   const newBook = new Book(title, author, pages, readStatus);
 
-  myLibrary.push(newBook);
+  // myLibrary.forEach((shelf, index) => {
+  // if (shelf[index].length < 7) {
+  //  shelf[index].push(newBook);
+  // } else {
+  // create a new shelf
+  // myLibrary.push([]);
+  // create a new container
+  // const cardsBlock = document.createElement('section');
+  // cardsBlock.classList.add("cards");
+  // document.body.appendChild(cardsBlock);
+  // }
+  //})
 
-  goThroughArray();
+  // isFull = true
+  // if isFull is true - don't execute
+  myLibrary.forEach((shelf, index) => {
+    if (shelf.length >= 8 && myLibrary.length === index + 1) {
+      myLibrary.push([]);
+    } else if (shelf.length >= 8) {
+      return;
+    } else {
+      shelf.push(newBook);
+      goThroughArray();
+    }
+  });
 }
+// if shelf.index > 8 {return}
+// shelf.push()
+// f checkIfArrayIsFull(shelf)
+// {if {shelf > 8} {return}}
+// if myLibrary.length === shelf.index - only in this case create a new array
 
 function goThroughArray() {
+  //
   myLibrary.forEach((book, index) => {
     const bookCardBody = document.createElement("article");
     const title = document.createElement("h2");
@@ -67,8 +97,14 @@ function goThroughArray() {
     bookCardBody.appendChild(removeBtn);
 
     bookCardBody.setAttribute("data-index", index);
+    // if (document.querySelectorAll(".book-card").length > 7) {
+    //   const newCardsBlock = document.createElement("section");
+    //   newCardsBlock.classList.add("cards");
+    //   document.body.appendChild(newCardsBlock);
+    //   newCardsBlock.appendChild(bookCardBody);
+    // } else {
     cardsBlock.appendChild(bookCardBody);
-
+    // }
     statusInput.addEventListener("click", () => {
       if (document.querySelector(`#read${index}`).checked) {
         myLibrary[index].read = true;
@@ -76,6 +112,19 @@ function goThroughArray() {
         myLibrary[index].read = false;
       }
     });
+
+    // myLibrary.forEach((shelf, index) => {
+    // if (shelf[index].length < 7) {
+    //  shelf[index].push(newBook);
+    // } else {
+    // create a new shelf
+    // myLibrary.push([]);
+    // create a new container
+    // const cardsBlock = document.createElement('section');
+    // cardsBlock.classList.add("cards");
+    // document.body.appendChild(cardsBlock);
+    // }
+    //})
 
     removeBtn.addEventListener("click", (e) => {
       const currentCard = document.querySelector(
