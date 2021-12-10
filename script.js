507c6562cfda3eb755eb64a2ebca914293ce6c27
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-11 04:43:30 +0700

    adjusted the code that makes book cards apper on the page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let myLibrary = [[]];
 const cardsBlock = document.querySelector(".cards");
 const addBookBtn = document.getElementById("add-book");
-let isFull;
+// let isFull;
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -35,6 +35,15 @@ function addBookToLibrary() {
   // }
   //})
 
+  // nested array
+  // first layer for myLibrary
+  // second for shelfs
+  // for (let i = 0; i < myLibrary.length; i++) {
+  //  for (let j = 0; j <= 8; j++) {
+
+  // }
+  // }
+
   // isFull = true
   // if isFull is true - don't execute
   myLibrary.forEach((shelf, index) => {
@@ -53,88 +62,89 @@ function addBookToLibrary() {
 // f checkIfArrayIsFull(shelf)
 // {if {shelf > 8} {return}}
 // if myLibrary.length === shelf.index - only in this case create a new array
-
 function goThroughArray() {
-  //
-  myLibrary.forEach((book, index) => {
-    const bookCardBody = document.createElement("article");
-    const title = document.createElement("h2");
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
-    const inputLabelContainer = document.createElement("div");
-    const statusInput = document.createElement("input");
-    const statusLabel = document.createElement("label");
-    const removeBtn = document.createElement("button");
+  cardsBlock.replaceChildren();
+  for (let i = 0; i < myLibrary.length; i++) {
+    for (let j = 0; j < myLibrary[i].length; j++) {
+      const bookCardBody = document.createElement("article");
+      const title = document.createElement("h2");
+      const author = document.createElement("p");
+      const pages = document.createElement("p");
+      const inputLabelContainer = document.createElement("div");
+      const statusInput = document.createElement("input");
+      const statusLabel = document.createElement("label");
+      const removeBtn = document.createElement("button");
 
-    statusInput.setAttribute("type", "checkbox");
-    statusInput.setAttribute("id", "read" + index);
-    statusLabel.setAttribute("for", "read" + index);
-    removeBtn.setAttribute("data-index", index);
+      // statusInput.setAttribute("type", "checkbox");
+      // statusInput.setAttribute("id", "read" + index);
+      // statusLabel.setAttribute("for", "read" + index);
+      // removeBtn.setAttribute("data-index", index);
 
-    statusInput.checked = book.read;
+      statusInput.checked = myLibrary[i][j].read;
 
-    bookCardBody.classList.add("book-card");
-    title.classList.add("title");
-    author.classList.add("author");
-    pages.classList.add("pages");
-    inputLabelContainer.classList.add("input-and-label");
-    removeBtn.classList.add("remove-btn");
+      bookCardBody.classList.add("book-card");
+      title.classList.add("title");
+      author.classList.add("author");
+      pages.classList.add("pages");
+      inputLabelContainer.classList.add("input-and-label");
+      removeBtn.classList.add("remove-btn");
 
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
-    statusLabel.textContent = "Read";
-    removeBtn.textContent = "Remove the book";
+      title.textContent = myLibrary[i][j].title;
+      author.textContent = myLibrary[i][j].author;
+      pages.textContent = myLibrary[i][j].pages;
+      statusLabel.textContent = "Read";
+      removeBtn.textContent = "Remove the book";
 
-    bookCardBody.appendChild(title);
-    bookCardBody.appendChild(author);
-    bookCardBody.appendChild(pages);
+      bookCardBody.appendChild(title);
+      bookCardBody.appendChild(author);
+      bookCardBody.appendChild(pages);
 
-    inputLabelContainer.appendChild(statusInput);
-    inputLabelContainer.appendChild(statusLabel);
-    bookCardBody.appendChild(inputLabelContainer);
+      inputLabelContainer.appendChild(statusInput);
+      inputLabelContainer.appendChild(statusLabel);
+      bookCardBody.appendChild(inputLabelContainer);
 
-    bookCardBody.appendChild(removeBtn);
+      bookCardBody.appendChild(removeBtn);
 
-    bookCardBody.setAttribute("data-index", index);
-    // if (document.querySelectorAll(".book-card").length > 7) {
-    //   const newCardsBlock = document.createElement("section");
-    //   newCardsBlock.classList.add("cards");
-    //   document.body.appendChild(newCardsBlock);
-    //   newCardsBlock.appendChild(bookCardBody);
-    // } else {
-    cardsBlock.appendChild(bookCardBody);
-    // }
-    statusInput.addEventListener("click", () => {
-      if (document.querySelector(`#read${index}`).checked) {
-        myLibrary[index].read = true;
-      } else {
-        myLibrary[index].read = false;
-      }
-    });
+      // bookCardBody.setAttribute("data-index", index);
+      // if (document.querySelectorAll(".book-card").length > 7) {
+      //   const newCardsBlock = document.createElement("section");
+      //   newCardsBlock.classList.add("cards");
+      //   document.body.appendChild(newCardsBlock);
+      //   newCardsBlock.appendChild(bookCardBody);
+      // } else {
+      cardsBlock.appendChild(bookCardBody);
+      // }
+      // statusInput.addEventListener("click", () => {
+      //   if (document.querySelector(`#read${index}`).checked) {
+      //     myLibrary[index].read = true;
+      //   } else {
+      //     myLibrary[index].read = false;
+      //   }
+      // });
 
-    // myLibrary.forEach((shelf, index) => {
-    // if (shelf[index].length < 7) {
-    //  shelf[index].push(newBook);
-    // } else {
-    // create a new shelf
-    // myLibrary.push([]);
-    // create a new container
-    // const cardsBlock = document.createElement('section');
-    // cardsBlock.classList.add("cards");
-    // document.body.appendChild(cardsBlock);
-    // }
-    //})
+      // myLibrary.forEach((shelf, index) => {
+      // if (shelf[index].length < 7) {
+      //  shelf[index].push(newBook);
+      // } else {
+      // create a new shelf
+      // myLibrary.push([]);
+      // create a new container
+      // const cardsBlock = document.createElement('section');
+      // cardsBlock.classList.add("cards");
+      // document.body.appendChild(cardsBlock);
+      // }
+      //})
 
-    removeBtn.addEventListener("click", (e) => {
-      const currentCard = document.querySelector(
-        `article[data-index="${bookCardBody.dataset.index}"]`
-      );
-      cardsBlock.removeChild(currentCard);
-      myLibrary.splice(bookCardBody.dataset.index, 1);
-      test();
-    });
-  });
+      // removeBtn.addEventListener("click", (e) => {
+      //   const currentCard = document.querySelector(
+      //     `article[data-index="${bookCardBody.dataset.index}"]`
+      //   );
+      //   cardsBlock.removeChild(currentCard);
+      //   myLibrary.splice(bookCardBody.dataset.index, 1);
+      //   test();
+      // });
+    }
+  }
 }
 
 addBookBtn.addEventListener("click", (e) => {
