3a300104c281bd5e4b1b34855f54a7cf8baac208
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-13 20:46:10 +0700

    implemented deleting of objects, DOM elements and the array elements correspond to each other

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
+Book.prototype.addIndex = function () {
+  this.index = myLibrary.length;
+};
+
 function addBookToLibrary() {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
 
   const newBook = new Book(title, author, pages);
+  newBook.addIndex();
+  console.log(newBook);
 
   myLibrary.push(newBook);
 
@@ -28,9 +34,9 @@ function addBookToLibrary() {
 }
 
 function addBookToPage(container, book) {
-  // container.replaceChildren();
   const bookBody = document.createElement("article");
   bookBody.classList.add("book-card");
+  bookBody.setAttribute("data-index", book.index);
 
   const bookTitle = document.createElement("h2");
   bookTitle.classList.add("title");
@@ -38,17 +44,28 @@ function addBookToPage(container, book) {
 
   const bookAuthor = document.createElement("p");
   bookAuthor.classList.add("author");
-  bookTitle.textContent = book.author;
+  bookAuthor.textContent = book.author;
 
   const bookPages = document.createElement("p");
   bookPages.classList.add("pages");
-  bookTitle.textContent = book.pages;
+  bookPages.textContent = book.pages;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.textContent = "Remove the book";
 
   bookBody.appendChild(bookTitle);
   bookBody.appendChild(bookAuthor);
   bookBody.appendChild(bookPages);
+  bookBody.appendChild(deleteBtn);
 
   container.appendChild(bookBody);
+  console.log(bookBody);
+
+  deleteBtn.addEventListener("click", () => {
+    bookBody.remove();
+    delete myLibrary[book.index];
+  });
 }
 
 addBookBtn.addEventListener("click", (e) => {
