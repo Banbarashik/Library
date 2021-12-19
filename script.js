01cf1f662bc2c516aaf8cb3c6c81f57839d19cca
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-20 00:55:04 +0700

    made so that new shelves are added correctly

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-"use strict";
+'use strict';
 let myLibrary = [];
 let booksLocal = [];
-let cardsBlock = document.querySelector(".cards");
-const addBookBtn = document.getElementById("add-book");
-const bookshelf = document.getElementById("bookshelf");
-const bookForm = document.getElementById("book-form");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const totalPages = document.getElementById("totalPages");
-const completedPages = document.getElementById("completedPages");
-const errMessage = document.getElementById("error-message");
+// let cardsBlock = document.querySelector('.cards');
+let cardsBlock = document.querySelector('.cards');
+const addBookBtn = document.getElementById('add-book');
+const bookshelf = document.getElementById('bookshelf');
+const bookForm = document.getElementById('book-form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const totalPages = document.getElementById('totalPages');
+const completedPages = document.getElementById('completedPages');
+const errMessage = document.getElementById('error-message');
 
 (function () {
-  if (localStorage.getItem("books")) {
-    booksLocal = JSON.parse(localStorage.getItem("books"));
-    booksLocal.forEach((book) => {
+  if (localStorage.getItem('books')) {
+    booksLocal = JSON.parse(localStorage.getItem('books'));
+    booksLocal.forEach(book => {
       if (cardsBlock.childElementCount === 8) {
-        cardsBlock = document.createElement("section");
-        cardsBlock.classList.add("cards", "cards-sequence");
+        cardsBlock = document.createElement('section');
+        cardsBlock.classList.add('cards', 'cards-sequence');
         bookshelf.appendChild(cardsBlock);
       }
       addBookToPage(cardsBlock, book);
@@ -40,73 +41,96 @@ function Book(title, author, totalPages, completedPages) {
   ${getRandomNumber(50, 80)}%)`;
 }
 
-Book.prototype.addIndex = function () {
-  this.index = myLibrary.length;
-};
+// Book.prototype.addIndex = function () {
+//   this.index = myLibrary.length;
+// };
 
 function addBookToLibrary() {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const totalPages = document.getElementById("totalPages").value;
-  const completedPages = document.getElementById("completedPages").value;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const totalPages = document.getElementById('totalPages').value;
+  const completedPages = document.getElementById('completedPages').value;
 
   const newBook = new Book(title, author, totalPages, completedPages);
-  newBook.addIndex();
+  // newBook.addIndex();
 
   myLibrary.push(newBook);
-  booksLocal.push(newBook);
-  localStorage.setItem("books", JSON.stringify(booksLocal));
+  // booksLocal.push(newBook);
+  // localStorage.setItem('books', JSON.stringify(booksLocal));
 
-  if (cardsBlock.childElementCount === 8) {
-    cardsBlock = document.createElement("section");
-    cardsBlock.classList.add("cards", "cards-sequence");
-    bookshelf.appendChild(cardsBlock);
-  }
+  // if (cardsBlock.childElementCount === 8) {
+  //   cardsBlock = document.createElement('section');
+  //   cardsBlock.classList.add('cards', 'cards-sequence');
+  //   bookshelf.appendChild(cardsBlock);
+  // }
 
-  addBookToPage(cardsBlock, newBook);
+  const cardsContainers = document.querySelectorAll('.cards');
+  cardsContainers.forEach((container, index) => {
+    container.replaceChildren();
+    if (index) {
+      container.remove();
+    }
+  });
+
+  // addBookToPage(cardsBlock, newBook);
+  // cardBlocks.forEach(block => {
+  // block.replaceChildren();
+  myLibrary.forEach((book, index) => {
+    if (cardsBlock.childElementCount === 8) {
+      cardsBlock = document.createElement('section');
+      cardsBlock.classList.add('cards', 'cards-sequence');
+      bookshelf.appendChild(cardsBlock);
+    }
+
+    addBookToPage(cardsBlock, book, index);
+  });
+  cardsBlock = document.querySelector('.cards');
+  // });
+  // cardsBlock.replaceChildren();
 }
 
-function addBookToPage(container, book) {
-  const bookBody = document.createElement("article");
-  bookBody.classList.add("book-card");
-  bookBody.setAttribute("data-index", book.index);
+function addBookToPage(container, book, index) {
+  const bookBody = document.createElement('article');
+  bookBody.classList.add('book-card');
+  // bookBody.setAttribute("data-index", book.index);
+  bookBody.setAttribute('data-index', index);
   bookBody.style.backgroundColor = book.color;
 
-  const bookTitle = document.createElement("h2");
-  bookTitle.classList.add("title");
+  const bookTitle = document.createElement('h2');
+  bookTitle.classList.add('title');
   bookTitle.textContent = book.title;
 
-  const bookAuthorPagesBlock = document.createElement("div");
-  bookAuthorPagesBlock.classList.add("author-and-pages");
+  const bookAuthorPagesBlock = document.createElement('div');
+  bookAuthorPagesBlock.classList.add('author-and-pages');
 
-  const bookAuthor = document.createElement("p");
-  bookAuthor.classList.add("author");
+  const bookAuthor = document.createElement('p');
+  bookAuthor.classList.add('author');
   bookAuthor.textContent = book.author;
 
-  const bookTotalPages = document.createElement("p");
-  bookTotalPages.classList.add("totalPages");
-  bookTotalPages.textContent = book.totalPages + " pages";
+  const bookTotalPages = document.createElement('p');
+  bookTotalPages.classList.add('totalPages');
+  bookTotalPages.textContent = book.totalPages + ' pages';
 
   bookAuthorPagesBlock.appendChild(bookAuthor);
   bookAuthorPagesBlock.appendChild(bookTotalPages);
 
-  const completedPagesBlock = document.createElement("div");
-  completedPagesBlock.classList.add("completed-pages-wrapper");
+  const completedPagesBlock = document.createElement('div');
+  completedPagesBlock.classList.add('completed-pages-wrapper');
 
-  const bookCompletedPages = document.createElement("input");
-  bookCompletedPages.classList.add("completed-pages");
+  const bookCompletedPages = document.createElement('input');
+  bookCompletedPages.classList.add('completed-pages');
   bookCompletedPages.value = book.completedPages;
-  bookCompletedPages.setAttribute("type", "text");
-  bookCompletedPages.setAttribute("maxlength", String(book.totalPages).length);
+  bookCompletedPages.setAttribute('type', 'text');
+  bookCompletedPages.setAttribute('maxlength', String(book.totalPages).length);
 
   completedPagesBlock.appendChild(bookCompletedPages);
 
-  setInputFilter(bookCompletedPages, function (value) {
-    return /^\d*?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
-  });
+  // setInputFilter(bookCompletedPages, function (value) {
+  //   return /^\d*?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
+  // });
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("delete-btn");
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete-btn');
 
   bookBody.appendChild(bookTitle);
   bookBody.appendChild(bookAuthorPagesBlock);
@@ -115,34 +139,27 @@ function addBookToPage(container, book) {
 
   container.appendChild(bookBody);
 
-  deleteBtn.addEventListener("click", () => {
+  deleteBtn.addEventListener('click', () => {
     bookBody.remove();
 
     delete myLibrary[book.index];
 
-    // const booksLocal = JSON.parse(localStorage.getItem("books"));
     booksLocal.forEach((bookLocal, index) => {
       if (bookLocal.index === book.index) {
         booksLocal.splice(index, 1);
-        localStorage.setItem("books", JSON.stringify(booksLocal));
+        localStorage.setItem('books', JSON.stringify(booksLocal));
       }
     });
 
     if (
       cardsBlock.childElementCount === 0 &&
-      bookshelf.getElementsByClassName("cards").length !== 1
+      bookshelf.getElementsByClassName('cards').length !== 1
     ) {
       cardsBlock.remove();
       cardsBlock = bookshelf.lastElementChild;
     }
 
-    // if (cardsBlock.previousElementSibling.childElementCount < 8) {
-    //   cardsBlock.previousElementSibling.appendChild(cardsBlock.lastChild);
-    // }
-    // if (cardsBlock.previousElementSibling.childElementCount < 8) {
-    //   cardsBlock.previousElementSibling.appendChild(cardsBlock.lastChild);
-    // }
-    const allBlocks = document.querySelectorAll(".cards");
+    const allBlocks = document.querySelectorAll('.cards');
     allBlocks.forEach((block, index) => {
       if (index && block.previousElementSibling.childElementCount < 8) {
         block.previousElementSibling.appendChild(block.firstChild);
@@ -150,48 +167,52 @@ function addBookToPage(container, book) {
     });
   });
 
-  bookCompletedPages.addEventListener("blur", (e) => {
+  bookCompletedPages.addEventListener('blur', e => {
     const max = book.totalPages;
     if (parseInt(e.target.value) > max) e.target.value = max;
-    if (parseInt(e.target.value) === 0 || e.target.value === "")
+    if (parseInt(e.target.value) === 0 || e.target.value === '')
       e.target.value = 1;
     book.completedPages = e.target.value;
 
-    localStorage.setItem("books", JSON.stringify(booksLocal));
+    localStorage.setItem('books', JSON.stringify(booksLocal));
+  });
+
+  setInputFilter(bookCompletedPages, function (value) {
+    return /^\d*?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
   });
 }
 
 function setInputFilter(textbox, inputFilter) {
   [
-    "input",
-    "keydown",
-    "keyup",
-    "mousedown",
-    "mouseup",
-    "select",
-    "contextmenu",
-    "drop",
+    'input',
+    'keydown',
+    'keyup',
+    'mousedown',
+    'mouseup',
+    'select',
+    'contextmenu',
+    'drop',
   ].forEach(function (event) {
     textbox.addEventListener(event, function () {
       if (inputFilter(this.value)) {
         this.oldValue = this.value;
         this.oldSelectionStart = this.selectionStart;
         this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
+      } else if (this.hasOwnProperty('oldValue')) {
         this.value = this.oldValue;
         this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
       } else {
-        this.value = "";
+        this.value = '';
       }
     });
   });
 }
 
-addBookBtn.addEventListener("click", (e) => {
+addBookBtn.addEventListener('click', e => {
   e.preventDefault();
 
-  if (title.value === "" || author.value === "" || totalPages.value === "") {
-    errMessage.textContent = "Please, fill out all required fields.";
+  if (title.value === '' || author.value === '' || totalPages.value === '') {
+    errMessage.textContent = 'Please, fill out all required fields.';
     return false;
   } else if (
     parseInt(totalPages.value) < parseInt(totalPages.min) ||
@@ -204,17 +225,17 @@ addBookBtn.addEventListener("click", (e) => {
     completedPages.value = totalPages.value;
   } else if (
     parseInt(completedPages.value) === 0 ||
-    completedPages.value === ""
+    completedPages.value === ''
   ) {
     completedPages.value = 1;
   }
 
   addBookToLibrary();
 
-  errMessage.textContent = "";
+  errMessage.textContent = '';
 
-  title.value = "";
-  author.value = "";
-  totalPages.value = "";
-  completedPages.value = "";
+  title.value = '';
+  author.value = '';
+  totalPages.value = '';
+  completedPages.value = '';
 });
